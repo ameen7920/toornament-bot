305bf0099d5f9312dcb2dc0f367ba0f25a8ee5d0
Anton Egorov <anton.egoroff@gmail.com>
2016-02-07 00:35:32 +0300

    create confirmation code

--- tools/link_participants.py
+++ tools/link_participants.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import csv
+import hashlib
+import hmac
 
 import boto3
 import click
@@ -9,6 +11,10 @@ from client import Client
 
 @click.command()
 @click.argument('export-csv', type=click.File('rb'), required=False)
+@click.option('--bot-name', envvar='TELEGRAM_BOT_NAME', help=(
+    'Telegram Bot name. More about bots here https://core.telegram.org/bots. '
+    'You can set default value using TELEGRAM_BOT_NAME env variable.'
+))
 @click.option('--api-key', envvar='TOORNAMENT_API_KEY', help=(
     'API key for Toornament API. Create new Application on '
     'https://developer.toornament.com/applications to obtain a key. You can '
@@ -24,7 +30,11 @@ from client import Client
     'default value using PARTICIPANTS_TABLE env variable.'
 
 ))
-def link(export_csv, api_key, tournament_id, table):
+@click.option('--secret', envvar='SECRET_KEY', prompt=True, hide_input=True, help=(
+    'Secret key to use as salt for confirmation code. You can set default '
+    'value using SECRET_KEY env variable.'
+))
+def link(export_csv, bot_name, api_key, tournament_id, table, secret):
     emails = {row['Player/Team name'].decode('utf-8'): row['Player email']
               for row in csv.DictReader(export_csv)} if export_csv else {}
 
@@ -32,17 +42,30 @@ def link(export_csv, api_key, tournament_id, table):
     users = dynamodb.Table(table)
 
     for p in Client(api_key).list_participants(tournament_id):
+        digest = hmac.new(key=str(secret), digestmod=hashlib.sha256)
+        digest.update(api_key)
+        digest.update(p['id'])
+        code = digest.hexdigest()[:32]
+
         name = p['name']
         users.put_item(Item={
             'participant_id': p['id'],
             'tournament_id': tournament_id,
             'name': name,
-            'foo': 2,
+            'confimation_code': code,
         })
         if name not in emails:
-            msg = u'Unknown email for {u}'.format(u=name)
+            msg = u'Unknown email for {u} share following link manually with this user'.format(u=name)
             click.echo(click.style(msg, fg='red'), err=True)
-        click.echo(u'{u} {i}'.format(u=name, i=p['id']))
+        else:
+            # TODO send email
+            pass
+        click.echo(u'{u}: {link}'.format(u=name, link=start_url(bot_name, code)))
+
+
+def start_url(bot, code):
+    return 'https://telegram.me/{bot}?start={code}'.format(bot=bot, code=code)
+
 
 if __name__ == '__main__':
     link()

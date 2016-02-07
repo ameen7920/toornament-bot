492efcc9994aaa4aeb56214c60e77c5c001c7a2a
Anton Egorov <anton.egoroff@gmail.com>
2016-02-07 20:18:24 +0300

    lambda function

--- app/function.py
+++ app/function.py
@@ -1,6 +1,53 @@
+from __future__ import print_function, unicode_literals
+
+import boto3
+import re
 from boto3.dynamodb.conditions import Key
-from .toornament import Client
+
 from .constants import *  # noqa
+from .telegram import send_message
+from .toornament import Client
+
+
+def lambda_handler(event, context):
+    tid = event['tournament_id']
+    table = boto3.resource('dynamodb').Table('players')
+
+    message = event.get('message', {})
+    text = message.get('text', '').strip()
+    chat_id = int(message['chat']['id'])
+
+    start = re.match(r'^/start( \w+)?$', text)
+    if start:
+        code = (start.group(1) or '').strip()
+        t = 'Well met!'
+        if code:
+            response = table.query(
+                IndexName='confirmation',
+                Select='ALL_PROJECTED_ATTRIBUTES',
+                KeyConditionExpression=Key('confirmation_code').eq(code)
+            )
+            count = response['Count']
+            if count > 1:
+                t = 'Shit happend! Confirmation code is not unique!'
+            elif count == 0:
+                t = 'Invalid confirmation code ' + code
+            else:
+                i = response['Items'][0]
+                table.update_item(
+                    Key={'participant_id': i['participant_id']},
+                    UpdateExpression='SET chat_id = :chat_id',
+                    ExpressionAttributeValues={':chat_id': chat_id}
+                )
+                t = 'Graz! You\'re in!'
+    elif text.startswith('/widget'):
+        t = 'https://widget.toornament.com/tournaments/{id}/'.format(id=tid)
+    elif text.startswith('/matches'):
+        t = '\n'.join([m for m in get_matches(event['api_key'], table)])
+
+    if t:
+        send_message(event['bot_token'], chat_id, t)
+    return {'ok': True}
 
 
 def get_matches(chat_id, api_key, table):
---
+++ app/telegram.py
@@ -0,0 +1,16 @@
+import json
+import urllib2
+
+
+def send_message(token, chat_id, text):
+    url = 'https://api.telegram.org/bot{t}/sendMessage'.format(t=token)
+    req = urllib2.Request(url, data=json.dumps({
+        'chat_id': chat_id,
+        'text': text,
+        'disable_web_page_preview': True,
+        'parse_mode': 'Markdown',
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+    res = urllib2.urlopen(req)
+    return json.loads(res.read())

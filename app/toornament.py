4e98193e205d79ddfd1bc2537b374329ca9074ab
Anton Egorov <anton.egoroff@gmail.com>
2016-02-07 19:50:38 +0300

    list matches api call

--- app/toornament.py
+++ app/toornament.py
@@ -1,4 +1,5 @@
 import json
+import urllib
 import urllib2
 
 
@@ -7,13 +8,20 @@ class Client(object):
         self.api_key = api_key
         assert self.api_key
 
-    def call(self, path):
+    def call(self, path, query):
         url = 'https://api.toornament.com/v1/{p}'.format(p=path.strip('/'))
+        if query:
+            url += '?' + urllib.urlencode(query)
         req = urllib2.Request(url, headers={
             'X-Api-Key': self.api_key,
         })
         res = urllib2.urlopen(req)
         return json.loads(res.read())
 
-    def list_participants(self, tid):
-        return self.call('tournaments/{id}/participants'.format(id=tid))
+    def list_participants(self, tournamet_id):
+        return self.call('tournaments/{id}/participants'.format(id=tournamet_id))
+
+    def list_matches(self, tournamet_id, participant_id):
+        return self.call('tournaments/{id}/matches'.format(id=tournamet_id), {
+            'participant_id': participant_id
+        })
